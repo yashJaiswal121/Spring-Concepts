be577e94861795963869449d5d33e5393d27194e
unknown <LPT-2585@LPT2585.newgen.co.in>
2021-09-01 18:42:45 +0530

    wip

--- BeanScope/src/luv2code_beanscopes/BaseballCoach.java
+++ BeanScope/src/luv2code_beanscopes/BaseballCoach.java
@@ -1,11 +1,17 @@
 package luv2code_beanscopes;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class BaseballCoach implements Coach{
 	
 	public BaseballCoach(){
 		System.out.println("Default constructor : Baseball");
 	}
 	
+//	@PostConstruct   //-> Singleton Scope->So called only once while bean creation...
+	//equivalent to 'init-method' in xml config
+	//Will be called after contructor and DI
 	public void setupBaseballCoach(){
 		System.out.println("Setting up Baseball coach");
 	}
@@ -15,6 +21,20 @@ public class BaseballCoach implements Coach{
 		return "getDailyWorkout from BaseballCoach";
 	}
 	
+//	@PreDestroy          //-> Singleton Scope->So called only once just before bean destruction on container closing...
+	//equivalent to 'destroy-method' in xml config
+	//Note: Donot work with Prototype bean
+	/*
+	 * In contrast to the other scopes, Spring does not manage the complete lifecycle of a
+		prototype bean: the container instantiates, configures, and otherwise assembles a
+		prototype object, and hands it to the client, with no further record of that prototype
+		instance.
+		
+		Thus, although initialization lifecycle callback methods are called on all objects 
+		regardless of scope, in the case of prototypes, configured destruction lifecycle callbacks
+		 are not called. The client code must clean up prototype-scoped objects and release
+		  expensive resources that the prototype bean(s) are holding.   
+	 * */
 	public void destroyBaseballCoach(){
 		System.out.println("Destroying Baseball coach");
 	}
